a0c8ea526e2f3ce45d00fbd4312d599b5bd12e3a
lukasamueller@icloud.com <millennium1412@yahoo.co.jp>
2016-05-15 09:50:04 +0200

    Adding the Line model

---
+++ Sobyo/Line.swift
@@ -0,0 +1,21 @@
+//
+//  Line.swift
+//  Sobyo
+//
+//  Created by Lukas A. Müller on 15/05/16.
+//  Copyright © 2016 Lukas Müller. All rights reserved.
+//
+
+import UIKit
+
+class Line {
+    
+    let startingPoint: CGPoint
+    let endingPoint: CGPoint
+    
+    init(start: CGPoint, end: CGPoint) {
+        startingPoint = start
+        endingPoint = end
+    }
+    
+}
