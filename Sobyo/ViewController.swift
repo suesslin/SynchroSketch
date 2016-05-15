bd945def4cf437cbdeaa723282b98e8ce28d5c05
lukasamueller@icloud.com <millennium1412@yahoo.co.jp>
2016-05-15 09:47:27 +0200

    Initial Commit

---
+++ Sobyo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Sobyo
+//
+//  Created by Lukas A. Müller on 15/05/16.
+//  Copyright © 2016 Lukas Müller. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
